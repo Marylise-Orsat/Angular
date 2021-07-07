747bea613ed6a5c30180b6c3c10c882d592014de
Orsat-Marylise <bwb.orsat.m@fondespierre.com>
2021-07-07 15:06:03 +0000

    Modif messages

--- src/app/messaging/messages-list/messages-list.component.ts
+++ src/app/messaging/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { Message } from '../models/message';
 
 @Component({
@@ -8,44 +8,30 @@ import { Message } from '../models/message';
 })
 export class MessagesListComponent implements OnInit {
 
-  messages: Message[] = [
-    {
-      id: 12,
-      from: 'Mr Bean',
-      to: 'Mr Hi',
-      body: 'tutututu',
-      read: false,
-      subject: 'Important'
-    },
-    {
-      id: 13,
-      from: 'Michmich',
-      to: 'Colette',
-      body: 'totototo',
-      read: true,
-      subject: 'Hola'
-    },
-    {
-      id: 14,
-      from: 'Papa',
-      to: 'Maman',
-      body: 'titititi',
-      read: false,
-      subject: 'Ohlele'
-    },
-    {
-      id: 15,
-      from: 'Tata',
-      to: 'Tonton',
-      body: 'tatatata',
-      read: true,
-      subject: 'Ohlila'
-    }
-  ];
+  @Input() messages!: Message[];
+
+  // affiche le nb de messages que l'on veut dans notre component
+  @Input() count: number = 0;
+
 
   constructor() { }
 
   ngOnInit(): void {
+
+    //gestion du nombre de messages
+    this.messages = this.messages.splice(0, this.count);
+
+  }
+
+  // méthode qui récupère l'évenement du click sur le checkbox du read
+  filter(event: Event) {
+    // caste de l'élément afin de pouvoir le manipuler en objet
+    let input = event.target as HTMLInputElement;
+    // Va tester chaque message de la méthode filtrer
+    // renvoie l'élément si la condition est vérifiée : si l'input est coché
+    // et renvoie la liste des messages lus
+    this.messages.filter( message => message.read === input.checked);
+    console.log(event);
   }
 
 }
--- src/app/templates/content/content.component.ts
+++ src/app/templates/content/content.component.ts
@@ -1,3 +1,4 @@
+import { Message } from './../../messaging/models/message';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  discord!: Message[];
+  gmail!: Message[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.discord = [
+      {
+        id: 12,
+        from: 'Mr Bean',
+        to: 'Mr Hi',
+        body: 'tutututu',
+        read: false,
+        subject: 'Important'
+      },
+      {
+        id: 13,
+        from: 'Michmich',
+        to: 'Colette',
+        body: 'totototo',
+        read: true,
+        subject: 'Hola'
+      },
+      {
+        id: 14,
+        from: 'Papa',
+        to: 'Maman',
+        body: 'titititi',
+        read: false,
+        subject: 'Ohlele'
+      },
+      {
+        id: 15,
+        from: 'Tata',
+        to: 'Tonton',
+        body: 'tatatata',
+        read: true,
+        subject: 'Ohlila'
+      }
+    ];
+
+    this.gmail = [
+      {
+        id: 12,
+        from: 'Mr Bean',
+        to: 'Mr Hi',
+        body: 'tutututu',
+        read: false,
+        subject: 'Important'
+      },
+      {
+        id: 13,
+        from: 'Michmich',
+        to: 'Colette',
+        body: 'totototo',
+        read: true,
+        subject: 'Hola'
+      },
+      {
+        id: 14,
+        from: 'Papa',
+        to: 'Maman',
+        body: 'titititi',
+        read: false,
+        subject: 'Ohlele'
+      },
+      {
+        id: 15,
+        from: 'Tata',
+        to: 'Tonton',
+        body: 'tatatata',
+        read: true,
+        subject: 'Ohlila'
+      }
+    ];
   }
 
 }
